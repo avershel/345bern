c437782d84c1b5c3e11f9c8db0b2646eddf20fce
avershel <austinv@digitalinfuzion.com>
2015-10-21 17:06:49 -0400

    working multi word search

--- database/src/database/database.java
+++ database/src/database/database.java
@@ -99,48 +99,7 @@ public class database {
 	
 	public String search(String query)
 	{
-		String[] firstWord = query.split(" ");
+		return new search(query).results();
 		
-		//multi word query
-		if(firstWord.length > 1)
-		{
-			boolean check = false;
-			bucket b = buckets.get(firstWord[0]);
-			bucket queryb = new bucket(query);
-			for(paragraphObject po : b)
-			{
-				String[] words = po.toString().replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
-				for(int i = 0; i < words.length; i++)
-				{
-					if(words[i].equals(firstWord[0]) && i < words.length + firstWord.length)
-					{
-						for(int ii = 1; ii < firstWord.length; ii++)
-						{
-							if(!words[i+1].equals(firstWord[i]))
-							{
-								check = false;
-								break;
-								//second word matches
-							}
-							else
-							{
-								check = true;
-							}
-						}
-						if(check)
-						{
-							queryb.addParObject(po);
-						}
-					}
-				}
-			}
-			return queryb.toString();
-		}
-		//single word query
-		else
-		{
-			//System.out.println("QUERY ------> " + query);
-			return buckets.get(query).toString();
-		}
 	}
 }
