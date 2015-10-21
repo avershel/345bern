9856366d351916c5528baa719f2e91665e3bd036
avershel <austinv@digitalinfuzion.com>
2015-10-21 16:19:50 -0400

    working 1 word search

--- database/src/database/database.java
+++ database/src/database/database.java
@@ -1,7 +1,9 @@
-package database;
+package gutenSearch;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 //import java.util.Collection;
 import java.util.HashMap;
@@ -16,13 +18,84 @@ public class database {
 	
 	public void addTxtFiles(String filename) throws FileNotFoundException, IOException
 	{
-		addTxtFiles(new File(filename));
+		parseText(readFile(filename));
+	}
+
+	
+	public static String readFile(String fileName) throws IOException 
+	{
+	    BufferedReader buffread = new BufferedReader(new FileReader(fileName));
+	    try 
+	    {
+	        StringBuilder stringbuild = new StringBuilder();
+	        String line = buffread.readLine();
+
+	        while (line != null) {
+	        	stringbuild.append(line);
+	        	stringbuild.append("\n");
+	            line = buffread.readLine();
+	        }
+	        return stringbuild.toString();
+	    } 
+	    finally 
+	    {
+	    	buffread.close();
+	    }
 	}
 	
-	public void addTxtFiles(File file) throws FileNotFoundException, IOException
+	/**
+	 * Takes in string object(the full text) 
+	 * extracts title, author, converts to paragraph objects
+	 * @param text - the full text file
+	 */
+	public static void parseText(String text) 
 	{
-		new textObject(file);
+		String author = "";
+		String title = "";
+		int index;
+	    String[] paragraphs = text.split("\n");
+	    for (String paragraph : paragraphs) 
+	    {
+	        String arr[] = paragraph.split(" ");
+	        if(arr[0].equals("Author:"))
+	        {
+	        	for(int i = 1; i<arr.length; i++)
+	        	{
+	        		author += arr[i];
+	        		author += " ";
+	        	}
+	        	//System.out.println("The Author is: " + author);
+	            
+	        }
+	        
+	        if(arr[0].equals("Title:"))
+	        {
+	        	for(int i = 1; i<arr.length; i++)
+	        	{
+	        		title += arr[i];
+	        		title += " ";
+	        	}
+	        	//System.out.println("The Title is: " + title);
+	        }
+	        
+	    }
+	    index = 1;
+	    for (String paragraph : paragraphs) 
+	    {
+	    	if(!paragraph.equals(""))
+	    	{
+	    		//new paragraph object
+	    		//set title
+	    		//set author
+	    		//set paragraph
+	    		new paragraphObject(paragraph, title, author, index);
+	    		index ++;
+	    		//System.out.println("Paragraph: " + paragraph.trim());
+	    
+	    	}
+	    }
 	}
+	
 	
 	public String search(String query)
 	{
@@ -66,6 +139,7 @@ public class database {
 		//single word query
 		else
 		{
+			//System.out.println("QUERY ------> " + query);
 			return buckets.get(query).toString();
 		}
 	}
