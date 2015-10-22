1ab60144d1d4c4e6437cd9e68b62f729993d941a
avershel <austinv@digitalinfuzion.com>
2015-10-22 02:41:05 -0400

    Update bucket.java

--- database/src/database/bucket.java
+++ database/src/database/bucket.java
@@ -1,26 +1,47 @@
-package gutenSearch;
+package theGutenSearch;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
+/*
+ * bucket
+ * A class that creates a bucket
+ * 
+ * @author group 7
+ */
 public class bucket implements Iterable<paragraphObject>{
 	public String key;
 	public Set<paragraphObject> parList = new HashSet<paragraphObject>();
 	public int size = 0;
 	
+	
+	/*
+	 * A paragraphObject constructor
+	 * @param bucketKey the key of the bucket
+	 */
 	public bucket(String bucketKey)
 	{
 		this.key = bucketKey;
 	}
 	
+	/*
+	 * A helper method for accepting paragraphObjects
+	 * 
+	 * @param po the paragraphObject to add
+	 */
 	public void addParObject(paragraphObject po)
 	{
 		this.parList.add(po);
 		size++;
 	}
 	
+	/*
+	 * A helper method to convert the bucket to string
+	 * 
+	 * @return str the string representation of the bucket
+	 */
 	public String toString()
 	{
 		String str = "";
@@ -31,6 +52,11 @@ public class bucket implements Iterable<paragraphObject>{
 		return str;
 	}
 	
+	/*
+	 * A helper method to convert the bucket to string
+	 * 
+	 * @return parlist.size the size of the bucket
+	 */
 	public int length()
 	{
 		return parList.size();
