b376eec395a1c2c90eb4a36530f1d6713327bca9
avershel <austinv@digitalinfuzion.com>
2015-10-22 02:31:26 -0400

    final search1

--- database/src/database/search1.java
+++ database/src/database/search1.java
@@ -1,10 +1,15 @@
-package gutenSearch;
+package theGutenSearch;
 
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-
+/*
+ * search1
+ * A class that handles searching
+ * 
+ * @author group 7
+ */
 public class search1 {
 	
 	String [] termArray;
@@ -13,6 +18,11 @@ public class search1 {
 	String []expholder;
 	int index = 0;
 
+	/*
+	 * A search object constructor
+	 * 
+	 * @param query - the term(s) to be searched
+	 */
 	public search1(String query)
 	{
 		
@@ -45,6 +55,11 @@ public class search1 {
 
 	}
 	
+	/*
+	 * A helper method for parsing OR terms
+	 * 
+	 * @param exp - the expression to be parsed
+	 */
 	public void parseOr(String exp)
 	{
 		if(exp.indexOf('&') >= 0)
@@ -66,8 +81,11 @@ public class search1 {
 		
 	}
 	
-	//adds single word buckets to query
-	public void addPars(String exp)
+	/*
+	 * A helper method for parsing single terms
+	 * 
+	 * @param exp - the expression to be parsed
+	 */	public void addPars(String exp)
 	{
 		
 			bucket b = database.buckets.get(exp);
@@ -82,7 +100,12 @@ public class search1 {
 //				l2.clear();
 			}
 	}
-	
+
+		/*
+		 * A helper method for parsing AND terms
+		 * 
+		 * @param holder - an array containing joined expressions
+		 */
 	public void parseAnd(String[] holder)
 	{
 		List<paragraphObject> ll1 = new ArrayList<paragraphObject>();
@@ -128,7 +151,11 @@ public class search1 {
 
 	}
 	
-	
+	/*
+	 * A method to return the results of the search
+	 * 
+	 * @return a String representation of the search results
+	 */
 	public String results()
 	{
 		
@@ -150,6 +177,43 @@ public class search1 {
 		else
 		{
 			return "QUERY TERM NOT FOUND";
+		}
+	}
+	
+	/*
+	 * A helper method for parsing wildcard expressions
+	 * 
+	 * @param text - the expression to be parsed
+	 */
+	public static void wild(String text)
+	{
+		
+		for(int i = 0; i < text.length(); i++)
+		{
+			if(text.charAt(i) == '?')
+			{
+				for(char j = 'a'; j<='z';j++)
+				{
+					new search1(text.replace('?',j));
+				}
+			}
+		}
+	}
+	
+	/*
+	 * A helper method for parsing star expressions
+	 * 
+	 * @param text - the expression to be parsed
+	 */
+	public static void star(String text)
+	{
+		for(int i=0; i < text.length();i++)
+		{
+			if(text.charAt(i)=='*')
+			{
+				new search1(text.substring(0,i));
+				
+			}
 		}
 	}
 }
