821fd0dfc5e4ba7ff56919c64ec14e34b6d8f69c
avershel <austinv@digitalinfuzion.com>
2015-10-22 02:36:45 -0400

    Update paragraphObject.java

--- database/src/database/paragraphObject.java
+++ database/src/database/paragraphObject.java
@@ -1,5 +1,10 @@
-package gutenSearch;
-
+package theGutenSearch;
+/*
+ * paragraphObject
+ * A class that creates paragraph objects
+ * 
+ * @author group 7
+ */
 public class paragraphObject {
 	public String title;
 	public int ordinal;
@@ -7,6 +12,15 @@ public class paragraphObject {
 	public String paragraph;
 	public int arrindex;
 	
+	/*
+	 * A paragraphObject constructor
+	 * 
+	 * @param paragraph1 - the text of the paragraph
+	 * @param title1 - the title of the text
+	 * @param author1 - the author of the text
+	 * @param ord - the ordinal number of the paragraph
+	 * @param i - the array index of the paragraph
+	 */
 	public paragraphObject(String paragraph1, String title1, String author1, int ord, int i)
 	{
 		arrindex = i;
@@ -18,6 +32,10 @@ public class paragraphObject {
 
 	}
 	
+	/*
+	 * A helper method to parse the paragraph
+	 *
+	 */
 	public void parse()
 	{
 		String[] words = paragraph.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
@@ -28,6 +46,11 @@ public class paragraphObject {
 		}
 	}
 	
+	/*
+	 * A helper method to add text to buckets
+	 * 
+	 * @param key - the term(s) to be added
+	 */
 	public void addToBucket(String key)
 	{
 		bucket value = database.buckets.get(key);
@@ -46,40 +69,72 @@ public class paragraphObject {
 		}
 	}
 	
+	/*
+	 * A helper method to edit author
+	 * 
+	 * @param authorname - the new authors name
+	 */
 	public void editAuthor(String authorname)
 	{
 		author = authorname;
 	}
 	
+	/*
+	 * A helper method to edit title
+	 * 
+	 * @param titlename - the new title
+	 */
 	public void editTitle(String titleName)
 	{
 		title = titleName;
 	}
 	
+	/*
+	 * A helper method to get title
+	 * 
+	 */
 	public String getTitle()
 	{
 		return title;
 	}
 	
+	/*
+	 * A helper method to get author
+	 * 
+	 */
 	public String getAuthor()
 	{
 		return author;
 	}
 	
+	/*
+	 * A helper method to get the ordinal number
+	 * 
+	 */
 	public int getOrdinal()
 	{
 		return ordinal;
 	}
 	
+	/*
+	 * A helper method to get the text
+	 * 
+	 */
 	public String getPar()
 	{
 		return paragraph;
 	}
 	
+	/*
+	 * A helper method to convert the paragraphObject to string
+	 * 
+	 * @return str the string representation of the paragraphObject
+	 */
 	public String toString()
 	{
-		String str = String.format("Title: %s\tAuthor: %s\t Ordinal: %d\n", title, author, ordinal);
-		str += paragraph + "\n";
+		String str = String.format("Title: %s\nAuthor: %s\nOrdinal: %d\n", title, author, ordinal);
+		//String []hold = paragraph.split(".");
+		str += paragraph + "\n\n";
 		return str;
 	}
 	
