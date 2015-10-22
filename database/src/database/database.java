6dfd2951b3e84c563db9441de2b2c543c4f18f01
avershel <austinv@digitalinfuzion.com>
2015-10-21 23:44:51 -0400

    fixed editBook

--- database/src/database/database.java
+++ database/src/database/database.java
@@ -10,15 +10,19 @@ import java.util.HashMap;
 
 public class database {
 	public static HashMap<String, bucket> buckets;
+	public static int arrindex = 0;
+	public static HashMap<String, paragraphObject> pomap;
 
 	public database()
 	{
 		buckets = new HashMap<String,bucket>();
+		pomap = new HashMap<String,paragraphObject>();
+
 	}
 	
-	public void addTxtFiles(File filename) throws FileNotFoundException, IOException
+	public void addTxtFiles(String filename) throws FileNotFoundException, IOException
 	{
-		parseText(readFile(filename.toString()));
+		parseText(readFile(filename));
 	}
 
 	
@@ -88,7 +92,11 @@ public class database {
 	    		//set title
 	    		//set author
 	    		//set paragraph
-	    		new paragraphObject(paragraph, title, author, index);
+	    		paragraphObject p0 = new paragraphObject(paragraph, title, author, index, arrindex);
+	    		pomap.put(Integer.toString(arrindex), p0);
+
+	    		p0.parse();
+	    		arrindex++;
 	    		index ++;
 	    		//System.out.println("Paragraph: " + paragraph.trim());
 	    
@@ -102,4 +110,42 @@ public class database {
 		return new search1(query).results();
 		
 	}
+	
+	
+	
+	/**
+	 * Edits the source title and author of paragraph objects that match the entered "title" and "author" strings.
+	 * if the Administrator wants to edit those attributes
+	 * 
+	 * @param title	-title to change
+	 * @param author	-author to change
+	 * @param newTitle	-the new title
+	 * @param newAuthor	-the new author
+	 */
+	public void editBook(String title, String author,String newTitle, String newAuthor)
+	{
+		paragraphObject po;
+		
+		for(String s : pomap.keySet())
+		{
+			po = pomap.get(s);
+			if(po.getTitle().equals(title) && po.getAuthor().equals(author))
+			{
+				if(newTitle == null || newTitle.length() > 0 || newTitle.equals(""))
+				{
+					pomap.get(po.arrindex).editTitle(newTitle);
+				}
+				if(newAuthor == null || newAuthor.length() > 0 || newAuthor.equals(""))
+				{
+					pomap.get(po.arrindex).editAuthor(newAuthor);
+				}
+			}
+		}
+	}
+	
+
+	
+	
+	
+	
 }
